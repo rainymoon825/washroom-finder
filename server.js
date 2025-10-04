bb3f14380c4f52d5cd93c4fb2dee3a734eec6fe5
longmanl <longmanluol.l@gmail.com>
2025-10-04 15:38:50 -0700

    test

--- server.js
+++ server.js
@@ -9,7 +9,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
+    apiKey: "AIzaSyBZQbAYIsg_F_JIaLAulZKd0_mUM6jRXqY",
+    authDomain: "test-314c9.firebaseapp.com",
+    projectId: "test-314c9",
+    storageBucket: "test-314c9.firebasestorage.app",
+    messagingSenderId: "228304607579",
+    appId: "1:228304607579:web:99ee5af6ce14cd4a89a27e",
+    measurementId: "G-P6ENT122PM",
+    databaseURL: "https://washroom-c736e-default-rtdb.firebaseio.com/"
 };
 
 // Initialize Firebase
