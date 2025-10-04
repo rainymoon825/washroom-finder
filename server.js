1c5c1b8acbe316c460c1f00cdee298948ac6db33
longmanl <longmanluol.l@gmail.com>
2025-10-04 14:48:17 -0700

    firebase stuff

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +15,11 @@ const firebaseConfig = {
   storageBucket: "test-314c9.firebasestorage.app",
   messagingSenderId: "228304607579",
   appId: "1:228304607579:web:99ee5af6ce14cd4a89a27e",
-  measurementId: "G-P6ENT122PM"
+  measurementId: "G-P6ENT122PM",
+  databaseURL: "https://washroom-c736e-default-rtdb.firebaseio.com/"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const database = getDatabase(app);
