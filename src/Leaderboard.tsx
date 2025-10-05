cf58793c466a880b0bcb7a68427cc5bd433078a2
Benjamin Koo <benjamink0935@gmail.com>
2025-10-04 20:13:12 -0700

    LB

--- src/Leaderboard.tsx
+++ src/Leaderboard.tsx
@@ -3,9 +3,18 @@ import "./App.css";
 function Leaderboard() {
   return (
     <>
-      <header>
-        <h1 className="Header">Leaderboard</h1>
-      </header>
+    <h1 className = "Leaderboard">Leaderboard</h1>
+      <h1 className = "LeaderboardFirst">1. u/BenTheUltimatePisser</h1>
+      <h1 className = "LeaderboardSecond">2. u/BenThePissper</h1>
+      <h1 className = "LeaderboardThird">3. u/BenThePooper</h1>
+      <h1 className = "LeaderboardOther">4. u/Normie</h1>
+      <h1 className = "LeaderboardOther">5. u/Normie</h1>
+      <h1 className = "LeaderboardOther">6. u/Normie</h1>
+      <h1 className = "LeaderboardOther">7. u/Normie</h1>
+      <h1 className = "LeaderboardOther">8. u/Normie</h1>
+      <h1 className = "LeaderboardOther">9. u/Normie</h1>
+      <h1 className = "LeaderboardOther">10. u/Normie</h1>
+      <h1>(and more...)</h1>
     </>
   );
 }
